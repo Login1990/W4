194c359d05acc795de4166196df987655fc49097
Login1990 <79404334+Login1990@users.noreply.github.com>
2023-07-07 16:06:37 +0800

    Initial commit

---
+++ routes/index.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get("/recipe/:food", (req,res,next) => {
+  let feedback = {
+    name: req.params.food,
+    instructions: ["boil water","beans","graveyard"],
+    ingridients: ["beans","marmite","blakc troufel oil"]
+  }
+  return res.json(feedback)
+})
+
+module.exports = router;
